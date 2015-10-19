b7829e087e89589f3003cbb4176453df6872265b
Aaron Snowberger <jekkilekki@gmail.com>
2015-10-19 13:13:30 +0900

    Midterm directions

---
+++ Midterm.java
@@ -0,0 +1,68 @@
+/*
+ * Midterm.java
+ * Write our normal 4-line documentation here (with your name, etc)
+ */
+
+// You will need a Scanner object
+
+/*
+ * Begin the class here
+ */
+public {
+  
+  /*
+   * Begin the main method here
+   */
+  public {
+   
+    // STEP 1: Create a NEW 'Test' object in a NEW 'Test.java' file (details below)
+    /*
+     * Details about the TEST object
+     * Feel free to CUT/COPY/PASTE these directions into your NEW file
+     * 
+     * Create (construct) a new Test object that will:
+     *     1) ask the User some (simple) questions (like "what color is the sky?")
+     *     2) check their answers
+     *     3) print out a grade based on their passing percent
+     * 
+     * All variables should be PRIVATE
+     * variables: 1. String array questions[]   => that holds 2+ questions for your "Test"
+     *            2. String array answers[]     => that hold the RIGHT answers for your "Test"
+     *            3. String array answersUser[] => that WILL hold 2+ answers for your "Test"
+     *            4. int score                  => that holds the total number of correct answers
+     *            5. double grade               => that holds the User's grade percentage
+     * 
+     * All methods should be PUBLIC
+     * methods:   1. askQuestions( Scanner ) that will:
+     *                a) accept a Scanner object from the main program
+     *                b) loop through the questions (use a FOR loop)
+     *                c) ask them to the user
+     *                d) read in user input
+     *                e) store the user input in the answers[] array
+     *            2. chkAnswers() that will:
+     *                a) loop through the answersUser[] array (use a WHILE loop)
+     *                b) compares each answersUser[] with answers[] (use IF/ELSE)
+     *                    i)   IF correct (the same), add 2 POINTS to their score
+     *                    ii)  ELSE IF correct but the wrong case (upper/lower), add 1 POINT to their score
+     *                    iii) ELSE (incorrect), SUBTRACT 1 point from their score
+     *            3. double calcGrade() that will:
+     *                a) RETURN a typecast (double) value of their score
+     *                b) divide the total score by the number of questions[] to find this
+     *            4. printGrade( double ) that will:
+     *                a) accept a numeric score (percent)
+     *                b) use a SWITCH statement to RETURN 'A', 'B', 'C', 'D', or 'F' based on the score
+     */
+    
+    // STEP 2: Initialize a Scanner() object here
+    
+    // STEP 3: Initialize your Test() object here
+    
+    // STEP 4: askQuestions( Scanner ) here - pass in your Scanner object
+    
+    // STEP 5: chkAnswers() here
+    
+    // STEP 6: Print the grade here < use System.out.println( printGrade( calcGrade() ) ); >
+    
+  } // END main method
+  
+} // END class
